6cbc102e30bb939241332b597feb5823cc3fb645
Nida Baptismo <nidabaptismo04@gmail.com>
2026-01-06 16:08:29 +0800

    users table

---
+++ database/migrations/2026_01_06_080726_create_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id(); // bigint unsigned auto_increment
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps(); // created_at & updated_at
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
